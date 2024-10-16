457333c80c5c9b49571878ccb14f44435ce3fde0
NandanTyagi <nandantyagi@gmail.com>
2024-10-16 15:29:28 +0200

    Remove .env file from gitignore and update nodemailer configuration

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -2,8 +2,6 @@ import nodemailer from "nodemailer";
 
 const email = process.env.EMAIL_FROM;
 const pass = process.env.APP_PASSWORD;
-// const email = 'nandantyagi@gmail.com';
-// const pass = 'xghpwxazlqzwukgb';
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
